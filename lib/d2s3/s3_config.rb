eb33b2bd5af738f2c2cb92889e17e2403c3735db
John Maxwell <jgwmaxwell@gmail.com>
2012-01-25 09:52:54 +0000

    Allow setting class vars through ENV

--- lib/d2s3/s3_config.rb
+++ lib/d2s3/s3_config.rb
@@ -5,23 +5,29 @@ module D2S3
     cattr_reader :access_key_id, :secret_access_key, :bucket
 
     def self.load_config
-      filename = "#{Rails.root}/config/amazon_s3.yml"
-      config = YAML.load_file(filename)
+      if ENV['AMAZON_ACCESS_KEY_ID'] && ENV['AMAZON_SECRET_ACCESS_KEY']
+        @@access_key_id = ENV['AMAZON_ACCESS_KEY_ID']
+        @@secret_access_key = ENV['AMAZON_SECRET_ACCESS_KEY']
+        @@bucket = ENV['AMAZON_BUCKET']
+      else
+        filename = "#{Rails.root}/config/amazon_s3.yml"
+        config = YAML.load_file(filename)
 
-      unless config
-        raise "Config object from #{filename} is nil"
-      end
+        unless config
+          raise "Config object from #{filename} is nil"
+        end
 
-      unless config[Rails.env]
-        raise "No environment #{Rails.env} found in #{filename}"
-      end
+        unless config[Rails.env]
+          raise "No environment #{Rails.env} found in #{filename}"
+        end
 
-      @@access_key_id     = config[Rails.env]['access_key_id']
-      @@secret_access_key = config[Rails.env]['secret_access_key']
-      @@bucket            = config[Rails.env]['bucket_name']
+        @@access_key_id     = config[Rails.env]['access_key_id']
+        @@secret_access_key = config[Rails.env]['secret_access_key']
+        @@bucket            = config[Rails.env]['bucket_name']
 
-      unless @@access_key_id && @@secret_access_key && @@bucket
-        raise "Please configure your S3 settings in #{filename}."
+        unless @@access_key_id && @@secret_access_key && @@bucket
+          raise "Please configure your S3 settings in #{filename}."
+        end
       end
     end
   end
